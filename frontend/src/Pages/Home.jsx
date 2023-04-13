a00a3fbf96ed2016830a932d9174f3e9e99253f4
alexisstdev <alexissanmiguel03@gmail.com>
2023-04-13 18:28:08 -0500

    message fix

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -66,7 +66,7 @@ export default function Home() {
             title='No search results'
           />
         ) : (
-          <RenderCards data={posts} title='No posts found' />
+          <RenderCards data={posts} />
         )}
       </div>
     </div>
